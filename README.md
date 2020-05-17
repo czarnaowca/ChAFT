6b5531524d01b0a75909eafd13093bd78eaa5dd6
Czarna Owca <czarna.owca.mail@gmail.com>
2020-05-17 21:33:38 +0200

    Publishing project files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ChAFT
 Check if website is accessible from TOR network
+
+# Requirements
+Python >= 3.6
+pip3 install -r requirements.txt
+
+# Usage
+python3 chaft.py -h
---
+++ chaft.py
@@ -0,0 +1,55 @@
+import requests
+import argparse
+from termcolor import colored
+
+
+def check_url(url):
+    """
+    Check URL if it is accessible from TOR network
+    :param url: URL to check
+    """
+    session = requests.session()
+    session.proxies = {"http": "socks5h://localhost:9050", "https": "socks5h://localhost:9050"}
+
+    headers = {"User-agent": "Mozilla 2.0"}
+
+    session.cookies.clear()
+
+    try:
+        r = session.get(url, headers=headers)
+
+        if r.status_code == 200:
+            print(f"[+] {url} \t {colored('ACCESSIBLE', 'green')}")
+        else:
+            print(f"[-] {url} \t {colored('NOT ACCESSIBLE', 'red')}")
+    except requests.exceptions.RequestException as exc:
+        print(f"[*] {url} \t {colored({exc}, 'yellow')}")
+
+
+def check_from_file(urls_file):
+    """
+    Parse file with URLs and check each of them
+    :param urls_file: File with URLs
+    """
+    with open(urls_file) as fp:
+        for url in fp:
+            check_url(url.replace("\n", ""))
+
+
+if __name__ in "__main__":
+    print("Author: Czarna Owca")
+    print("Source: https://github.com/czarnaowca/ChAFT")
+    print("Contact: czarna.owca.mail@gmail.com")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--url", help="Check if URL is accessible from the TOR network")
+    parser.add_argument("-f", "--file",
+                        help="Parse file with URLs and check if each URL is accessible from the TOR network")
+    args = parser.parse_args()
+
+    if args.url:
+        check_url(args.url)
+    elif args.file:
+        check_from_file(args.file)
+    else:
+        parser.print_help()
